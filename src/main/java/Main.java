0e51ab780c1a7ee3bb616396830c573d9fbd28f3
심승용 <seungyongshim@nexon.co.kr>
2022-05-18 11:12:01 +0900

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,11 @@ import java.util.function.Function;
 public class Main {
 
     public static void main(String[] args) {
+        Optional<String> a = null;
+        Optional<String> b = Optional.ofNullable(null);
+
+        System.out.println(a);
+        System.out.println(b);
     }
 
 }
